a31c230385e8307cd552360ecd3fd5e5969c7179
Vitor Fonseca <vitorfonsecamarques@gmail.com>
2023-01-03 14:12:36 -0300

    Incluindo PersonController e model Person

---
+++ RestWithAspNetUdemy/RestWithAspNetUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithAspNetUdemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? adress { get; set; }
+        public string? gender { get; set; }
+    }
+}
